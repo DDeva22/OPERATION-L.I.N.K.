c303557901c56205c619215b781706cbd40ad11a
danman15726 <danman15726@gmail.com>
2020-02-09 23:53:50 -0600

    installed npm packages: express, express-handlebars, mongoose, cheerio and axios. Created the server.js and added a '/scrape' .get method that scrapes the reuters.com website for US specific news.

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const exhbrs = require("express-handlebars");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const db = require("./models");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(`mongodb://localhost/ScrapeRepository`, { useNewUrlParser: true });
+
+app.get(`/scrape`, function(req, res){
+    axios.get(`https://www.reuters.com/news/us`).then(function(res){
+
+        const $ = cheerio.load(response.data);
+
+
+        $(`.FeedItemHeadline_headline`).each(function(i, element) {
+            let resObject = {};
+
+
+            resObject.title = $(this).children("a").text();
+            resObject.link = $(this).children("a").attr(`href`);
+
+
+            db.Article.create(resObject).then(function(data){
+                console.log(data);
+            });
+
+        });
+        res.send(`SCRAPING COMPLETE`);
+    });
+
+});
+// app.get(`/scrape`, function(req, res){
+//     axios.get(`https://www.reuters.com/news/us`). then(function(res){
+//         const $ = cheerio.load(response.data);
+
+
+
+//         $(`.`)
+
+
+
+//     })
+
+
+
+// });
