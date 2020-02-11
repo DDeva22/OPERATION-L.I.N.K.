c505007eabf211dce8f3903ca6725ace4b80b1b5
danman15726 <danman15726@gmail.com>
2020-02-11 16:13:40 -0600

    Added a Notes button along next to the Visit button for every card. Working through the logic to where if you save the note, the button and the body disappears. It should reappear whenever you click 'Notes', everything should reappear.

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,25 +1,60 @@
-console.log(`FRONT END JAVASCRIPT LOADED`);
-$(".container").append(`<p>FRONT END LOADED</p>`);
+$(document).ready(function(){
+    console.log(`FRONT END JAVASCRIPT LOADED`);
+    $(".container").append(`<p>FRONT END LOADED</p>`);
 
 
 
-$.getJSON(`/articles`, function(package){
-    console.log(package);
-    for( i = 0; i < package.length; i++){
-        $(`#card`).append(`<div class = "row"><div class="card" style="width: 18rem;">
-                                    <img class="card-img-top" src="${package[i].imgLink}" alt="Card image cap">
-                                    <div class="card-body">
-                                    
-                                    <p class="card-text">${package[i].title}</p>
-                                    <a href="${package[i].link}" class="btn btn-primary">Visit</a>
-                                    <a href="${package[i].link}" class="btn btn-primary">Notes</a>
-                                    </div>
-                                </div></div>`);
+    $.getJSON(`/articles`, function(package){
+        console.log(package);
+        for( i = 0; i < package.length; i++){
+            $(`#card`).append(`<div class="card" style="width: 18rem;">
+                                        <img class="card-img-top" src="${package[i].imgLink}" alt="Card image cap">
+                                        <div class="card-body">
+                                        
+                                        <p class="card-text" id = ${package[i]._id}>${package[i].title}</p>
+                                        <a href="${package[i].link}" class="btn btn-primary">Visit</a>
+                                        <a class="btn btn-primary" id = "notes" value = ${package[i]._id}>Notes</a>
+                                        </div>
+                                    </div>`);
 
 
-    }
-   
+        }
+    
+    });
+
+    $(`#card`).on("click","#notes", function(){
+        
+        console.log(`CLICKED`);
+        console.log($(this).attr(`value`));
+        $(`#${$(this).attr(`value`)}`).append(`<textarea id='bodyinput' name='body' value = '${$(this).attr(`value`)}'></textarea>`);
+        $(`#${$(this).attr(`value`)}`).append(`<button id='savenote' value = '${$(this).attr(`value`)}'>Save Note</button>`);
+
+        
+
+
+
+
+
+    });
+
+
+    $(`#card`).on("click","#savenote", function(){
+        console.log(`SAVED ${$(this).attr(`value`)}`);
+        $(`#${$(this).attr(`value`)}`).remove();
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
-
-
-
