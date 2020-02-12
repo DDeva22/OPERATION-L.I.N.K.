9c8fbca8ff0e9522a7fd95e1190cfa4a3a0fc00c
danman15726 <danman15726@gmail.com>
2020-02-12 14:07:54 -0600

    Deleted the appending code and replaced it with a modal. MUCH better. I had to also import the bootstrap javascript.

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -7,43 +7,41 @@ $(document).ready(function(){
     $.getJSON(`/articles`, function(package){
         console.log(package);
         for( i = 0; i < package.length; i++){
-            $(`#card`).append(`<div class="card" style="width: 18rem;">
-                                        <img class="card-img-top" src="${package[i].imgLink}" alt="Card image cap">
+            $(`.row`).append(`<div class = "adf col-md-3">
+                                    <div class="card" style="width: 18rem;">
+                                        <img class="card-img-top img-fluid" src="${package[i].imgLink}" alt="Card image cap">
                                         <div class="card-body">
-                                        
-                                        <p class="card-text" id = ${package[i]._id}>${package[i].title}</p>
-                                        <a href="${package[i].link}" class="btn btn-primary">Visit</a>
-                                        <a class="btn btn-primary" id = "notes" value = ${package[i]._id}>Notes</a>
+                                            
+                                            <p class="card-text" id = ${package[i]._id}>${package[i].title}</p>
+                                            <a href="${package[i].link}" class="btn btn-primary">Visit</a>
+                                            <a class="btn btn-primary" id = "notes" value = ${package[i]._id} data-toggle="modal" data-target="#Noted">Notes</a>
                                         </div>
-                                    </div>`);
+                                    </div>
+                                </div>`);
 
 
         }
     
     });
 
-    $(`#card`).on("click","#notes", function(){
+    $(`.row`).on("click","#notes", function(){
         
-        console.log(`CLICKED`);
+        
+        $(`#save`).attr("value", $(this).attr(`value`))
+
         console.log($(this).attr(`value`));
-        $(`#${$(this).attr(`value`)}`).append(`<textarea id='bodyinput' name='body' value = '${$(this).attr(`value`)}'></textarea>`);
-        $(`#${$(this).attr(`value`)}`).append(`<button id='savenote' value = '${$(this).attr(`value`)}'>Save Note</button>`);
-
-        
-
-
+        console.log($(`#save`).attr(`value`));
 
 
 
     });
 
-
-    $(`#card`).on("click","#savenote", function(){
-        console.log(`SAVED ${$(this).attr(`value`)}`);
-        $(`#${$(this).attr(`value`)}`).remove();
-
-
+    $(`.modal-footer`).on("click", `#save`, function(){
+        console.log(`SAVED`);
     });
+
+
+    
 
 
 
